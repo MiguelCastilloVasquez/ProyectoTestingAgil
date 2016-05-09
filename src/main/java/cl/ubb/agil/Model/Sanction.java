279e3a4596943ab09a7f529c00b0de9dd4c6b10f
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-05-09 13:04:41 -0300

    Pruebas ID Sanction

--- src/main/java/cl/ubb/agil/Model/Sanction.java
+++ src/main/java/cl/ubb/agil/Model/Sanction.java
@@ -2,4 +2,8 @@ package cl.ubb.agil.Model;
 
 public class Sanction {
 
+	
+	public int getID(){
+		return 1234;
+	}
 }
--- src/main/test/cl/ubb/agil/Model/SanctionTest.java
+++ src/main/test/cl/ubb/agil/Model/SanctionTest.java
@@ -1,5 +1,17 @@
 package cl.ubb.agil.Model;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class SanctionTest {
 
+	@Test
+	public void testReturnIDSanctionIs1234(){
+		Sanction sanction= new Sanction();
+		
+		int id = sanction.getID();
+		
+		assertThat(id,equalTo(1234));
+	}
+	
 }
