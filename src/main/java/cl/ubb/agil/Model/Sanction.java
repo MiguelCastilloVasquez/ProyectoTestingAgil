96e6e2c861d610e36dd643a2ffb47663988e4e30
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-05-09 13:11:24 -0300

    Pruebas ReturnID Completo Sanction

--- src/main/java/cl/ubb/agil/Model/Sanction.java
+++ src/main/java/cl/ubb/agil/Model/Sanction.java
@@ -2,8 +2,13 @@ package cl.ubb.agil.Model;
 
 public class Sanction {
 
+	private int id;
+	
+	public Sanction(int id){
+		this.id=id;
+	}
 	
 	public int getID(){
-		return 1234;
+		return id;
 	}
 }
--- src/main/test/cl/ubb/agil/Model/SanctionTest.java
+++ src/main/test/cl/ubb/agil/Model/SanctionTest.java
@@ -7,11 +7,20 @@ public class SanctionTest {
 
 	@Test
 	public void testReturnIDSanctionIs1234(){
-		Sanction sanction= new Sanction();
+		Sanction sanction= new Sanction(1234);
 		
 		int id = sanction.getID();
 		
 		assertThat(id,equalTo(1234));
+	}
+	
+	@Test 
+	public void testReturnIDSanctionIs4321(){
+		Sanction sanction= new Sanction(4321);
+		
+		int id = sanction.getID();
+		
+		assertThat(id,equalTo(4321));
 	}
 	
 }
